e6c96a37ad8f7b7d73e82d16f5d947df5702bd46
dperk6 <daniel@perklin.ca>
2020-12-02 15:43:36 -0500

    staking

--- migrations/2_qfi_token.js
+++ migrations/2_qfi_token.js
@@ -1,5 +1,6 @@
 const QFI = artifacts.require("QERC20.sol");
 const QAirdrop = artifacts.require("QAirdrop.sol");
+const QStaking = artifacts.require("QPoolStakingFactory.sol");
 const web3 = require('web3');
 
 module.exports = function (deployer) {
@@ -11,5 +12,12 @@ module.exports = function (deployer) {
         web3.utils.toWei('400000', 'ether'),
         Math.round(new Date().getTime() / 1000) + 2592000,
         QFI.address)
+  }).then(() => {
+    // Deploy Staking Factory contract with required parameters
+    return deployer.deploy(
+      QStaking,
+      QFI.address,
+      Math.round(new Date().getTime() / 1000) + 2592000
+    )
   })
 };
